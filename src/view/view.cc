242b08f4f98c432b437f7eff835f73d8a14bc87a
Effectus <panovsavelii@gmail.com>
2023-03-16 15:11:49 +0700

    Base struct project

---
+++ src/view/view.cc
@@ -0,0 +1,15 @@
+#include "view.h"
+#include "./ui_view.h"
+
+View::View(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::View)
+{
+    ui->setupUi(this);
+}
+
+View::~View()
+{
+    delete ui;
+}
+
